61639235019fbc581e80519a00af4e0ee7f15c35
Shanti Stewart <stewars2@oregonstate.edu>
2020-06-11 19:50:05 -0700

    Concatenated into a 2D array.

--- Code/main.py
+++ Code/main.py
@@ -26,10 +26,16 @@ play_obj.wait_done()
 # load audio files:
 sample_freq_1, x1 = scipy.io.wavfile.read(file_1)
 sample_freq_2, x2 = scipy.io.wavfile.read(file_2)
-# display sizes of data arrays:
+num_samples = x1.shape[0]
 print("Size of x1: ", end="")
 print(x1.shape)
 print("Size of x2: ", end="")
 print(x2.shape)
-# display sampling frequencies:
 print("Sampling frequencies of audio files: {0}, {1}\n".format(sample_freq_1, sample_freq_2))
+
+# concatenate audio files into a 2D array:
+X = np.zeros((2, num_samples))
+X[0] = x1
+X[1] = x2
+print("Size of X: ", end="")
+print(X.shape)
