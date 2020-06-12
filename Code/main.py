3760acf9ef5f307b7c2c5b241ea2cb5c6655451d
Shanti Stewart <stewars2@oregonstate.edu>
2020-06-11 19:37:26 -0700

    Updated main.py

--- Code/main.py
+++ Code/main.py
@@ -6,6 +6,8 @@ import scipy.io.wavfile
 import simpleaudio as sa
 
 
+print("\n")
+
 # audio file names:
 file_1 = 'microphone_1.wav'
 file_2 = 'microphone_2.wav'
@@ -21,5 +23,12 @@ play_obj.wait_done()
 """
 
 # load audio files:
-sample_freq_1, x_1 = scipy.io.wavfile.read(file_1)
-sample_freq_2, x_2 = scipy.io.wavfile.read(file_2)
+sample_freq_1, x1 = scipy.io.wavfile.read(file_1)
+sample_freq_2, x2 = scipy.io.wavfile.read(file_2)
+# display sizes of data arrays:
+print("Size of x1: ", end="")
+print(x1.shape)
+print("Size of x2: ", end="")
+print(x2.shape)
+# display sampling frequencies:
+print("Sampling frequencies of audio files: {0}, {1}\n".format(sample_freq_1, sample_freq_2))
