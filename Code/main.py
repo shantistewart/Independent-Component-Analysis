354831dcf8caa08003b0e56983740af354197ba7
Shanti Stewart <stewars2@oregonstate.edu>
2020-06-11 19:45:31 -0700

    Added plotting_functions.py -- not tested.

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plotter
 import scipy.io.wavfile
 import simpleaudio as sa
 
---
+++ Code/plotting/plotting_functions.py
@@ -0,0 +1,25 @@
+# This file contains several plotting functions for data visualization.
+
+
+import numpy as np
+import matplotlib.pyplot as plotter
+
+
+def plot_raw_signals(X, sample_freq):
+    num_sig = X.shape[0]
+    num_samples = X.shape[1]
+
+    # sample points of raw signals:
+    samples = np.arange(0, num_samples)
+    # actual time points of raw signals:
+    time = (1 / sample_freq) * samples
+
+    # create and format subplot:
+    fig, axes = plotter.subplots(num_sig, 1)
+    plotter.subplots_adjust(hspace=1)
+
+    for i in range(num_sig):
+        axes[i, 0].set_title('Original Signal x{0}(t)'.format(i+1))
+        axes[i, 0].plot(time, X[i])
+        axes[i, 0].set_xlabel('Time (s)')
+        axes[i, 0].set_ylabel('x{0}(t)'.format(i+1))
