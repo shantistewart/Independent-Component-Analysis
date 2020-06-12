2f73c544947fb0e683bcb86d463a4da81fe8cbdd
Shanti Stewart <stewars2@oregonstate.edu>
2020-06-11 19:33:10 -0700

    Started main.py

--- Code/main.py
+++ Code/main.py
@@ -2,5 +2,24 @@
 
 
 import numpy as np
-import scipy
+import scipy.io.wavfile
+import simpleaudio as sa
 
+
+# audio file names:
+file_1 = 'microphone_1.wav'
+file_2 = 'microphone_2.wav'
+
+"""
+# play audio files:
+wave_obj = sa.WaveObject.from_wave_file(file_1)
+play_obj = wave_obj.play()
+play_obj.wait_done()
+wave_obj = sa.WaveObject.from_wave_file(file_2)
+play_obj = wave_obj.play()
+play_obj.wait_done()
+"""
+
+# load audio files:
+sample_freq_1, x_1 = scipy.io.wavfile.read(file_1)
+sample_freq_2, x_2 = scipy.io.wavfile.read(file_2)
